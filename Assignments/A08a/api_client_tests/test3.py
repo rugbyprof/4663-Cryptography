508b74afc21d5baefe53e4d9399621afb5d62d56
rugbyprof <terry.griffin@msutexas.edu>
2020-12-06 14:12:59 -0600

    stuff

--- Assignments/A08a/api_client_tests/test3.py
+++ Assignments/A08a/api_client_tests/test3.py
@@ -4,11 +4,29 @@ https://requests.readthedocs.io/en/master/user/quickstart/
 import requests
 import json
 
+#pip install cryptography
+import cryptography
+# Used to Generate Keys
+# from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+#Used to Store Keys and Read in Keys
+from cryptography.hazmat.primitives import serialization
+
+#Used to do Encryption
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+
+
 TOKEN = '892db29a750c4bd0e87184c04db19237ece'
 UID = '8020'
 API = 'http://msubackend.xyz/api/?route='
 USERS = {}
 PUBKEYS = {}
+
+
+def loadPubKey(pubkey):
+    return serialization.load_pem_public_key(pubkey)
 
 def pubKey():
     global PUBKEYS
@@ -43,7 +61,7 @@ def getUsers():
 
     for user in users['data']:
         if user['uid'] in PUBKEYS:
-            user['pubkey'] = PUBKEYS[user['uid']]
+            user['pubkey'] = PUBKEYS[user['uid']]['pubkey']
             USERS[user['uid']] = user
 
 
@@ -87,14 +105,48 @@ if __name__ == '__main__':
     pubKey()
     getUsers()
 
-    print(USERS)
     active = getActive()
 
-    for a in active:
-        print(a)
+    # for a in active:
+    #     print(a)
 
-    result = postMessage("Hey sarah, thank's for making it. Again...",'5147600')
+    #result = postMessage("This is a plaintext message encrypted with public key 5147600",'5147600')
 
-    #print(result)
+
+    # this is me loading a public key from my key dictionary
+    pk = loadPubKey(USERS['5147600']['pubkey'].encode('utf-8'))
+
+    # this encrypts the encoded plaintext mesage with a users public key
+    encrypted = pk.encrypt(
+        "This is a plaintext message encrypted with public key 5147600".encode('utf-8'),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    print(encrypted)
+
+    # For testing, I load same persons private key from a file
+    with open('./keys/5147600.private.key', "rb") as key_file:
+        private_key = serialization.load_pem_private_key(
+            key_file.read(),
+            password=None
+        )
+    # I decrypt message encoded with public key
+    original_message = private_key.decrypt(
+        encrypted,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    # print decrypted message
+    print(original_message)
+
+
+
     #5147600
 
